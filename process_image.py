90a73e31c7cb3ecd8611e0f185e697398904039b
aminudin <amin4udin@gmail.com>
2020-03-16 00:26:56 +0700

    init

--- process_image.py
+++ process_image.py
@@ -1,5 +1,56 @@
 from cv2 import cv2
 import pytesseract
+import numpy as np
+
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
 
 
 def process_image(path=None):
@@ -7,6 +58,11 @@ def process_image(path=None):
        return {'err': True, 'message': 'Path Can`t be empty', 'data':''}
     else:
         img = cv2.imread(path)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        result = pytesseract.image_to_string(gray)
+        gray = get_grayscale(img)
+        threshs = thresholding(gray)
+        openings = erode(gray)
+        custom_config = r'--psm 6'
+        cv2.imwrite('./test.jpg', openings)
+        result = pytesseract.image_to_string(openings, config=custom_config)
+        print(result)
         return {'err': False, 'message':'successfully recognized image', 'data': result}
